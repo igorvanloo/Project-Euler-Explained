1ae0206aca35de36b20899d58847177e0c9980a7
igorvanloo <igorvanloo@gmail.com>
2021-07-31 21:27:05 +0800

    Problem 94 Updated

--- pe00094 - Almost Equilateral Triangles.py
+++ pe00094 - Almost Equilateral Triangles.py
@@ -41,7 +41,7 @@ Anwser:
 --- 0.00039196014404296875 seconds ---
 '''
 
-import time, math, eulerlib
+import time, math
 
 def is_quadratic(x):
     cube__root = (x**(1/2))
@@ -68,23 +68,23 @@ def compute1(): #Go through C check if it quadratic I think reverese way will be
             C += 1
     return total
 
-def compute2(): #Actually it turned out to be pretty much the same speed ... so lets go one step further
+def compute2(limit): #Actually it turned out to be pretty much the same speed ... so lets go one step further
     
     total = 0
     k = 1
     while True:
-        if k > math.sqrt(((10**9)/2 - 1)**2 - 1):
+        if k > math.sqrt(((limit)/2 - 1)**2 - 1):
             break
         
         C1 = (2*(math.sqrt(3*k*k + 1)) + 1)/3 #Roots of 3C^2 - 2C - 1 - 4k^2 = 0, 2A = C + 1
         if float(C1) == int(C1): #Test if C is an integer, if it is we have found a solution
             total += 3*C1 + 1
-            print(3*C1 + 1)
+            print(total)
             
         C = (2*(math.sqrt(3*k*k + 1)) - 1)/3 #Roots of 3C^2 + 2C - 1 - 4k^2 = 0, 2A = C - 1
         if float(C) == int(C): #Test if C is an integer, if it is we have found a solution
             total += 3*C - 1
-            print(3*C - 1)
+            print(total)
         k += 1
             
     return total
@@ -95,21 +95,26 @@ def compute(limit):
     
     while True:
         
-        C = (((7 - 4*math.sqrt(3))**n + (7 + 4*math.sqrt(3))**n + 1)/3) #Integer solutions of 3C^2 - 2C - 1 - 4k^2 = 0, 2A = C + 1
+        C = (((7 - 4*math.sqrt(3))**n + (7 + 4*math.sqrt(3))**n + 1)/3) 
+        #Integer solutions of 3C^2 - 2C - 1 - 4k^2 = 0, 2A = C + 1
         
-        C1 = (((2 + math.sqrt(3)) * (7 - 4*math.sqrt(3))**(n+1) - (math.sqrt(3) - 2)*(7 + 4*math.sqrt(3))**(n+1) - 1)/3) #Integer solutions of 3C^2 + 2C - 1 - 4k^2 = 0, 2A = C - 1)
+        C1 = (((2 + math.sqrt(3)) * (7 - 4*math.sqrt(3))**(n+1) + (2 - math.sqrt(3))*(7 + 4*math.sqrt(3))**(n+1) - 1)/3) 
+        #Integer solutions of 3C^2 + 2C - 1 - 4k^2 = 0, 2A = C - 1)
         
         perC = 3*C + 1
         perC1 = 3*C1 - 1
         
-        if perC > limit:
+        if perC > limit and perC1 > limit:
             break
+        
+        if perC < limit:
+            total += perC
+            print(perC)
             
-        total += perC
-        if perC1 > limit:
-            break
+        if perC1 < limit:
+            total += perC1
+            print(perC1)
             
-        total += perC1
         n += 1
         
     return int(total)
