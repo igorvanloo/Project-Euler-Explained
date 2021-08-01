a6eebf1de430ad1da0534e868302176cf0c8819e
igorvanloo <igorvanloo@gmail.com>
2021-08-01 22:20:37 +0800

    Problem 108 Updated

--- pe00108 - Diophantine reciprocals I.py
+++ pe00108 - Diophantine reciprocals I.py
@@ -20,16 +20,15 @@ let x = n + a < y = n + b
 => n(2n + a + b) = n^2 + na + nb + ab
 => n^2 = ab
 
-This means that ab are pair divisors of n^2, so this problem is reduced to finding the first n^2 with > 1000 divisors
+This means that ab are pair divisors of n^2, so this problem is reduced to finding the first n^2 with > 2000 divisors
 
 Note
 if n = p1^a * p2^b * p3^c * ...
-then d(n) = (a+1)(b+1)(c+1)...
+then d(n) = (a+1)(b+1)(c+1)... d(n) is the number of divisors
 
 => d(n^2) = (2a+1)(2b+1)(2c+1)
 
-So I go through n and calculate the number of divisors of n^2 if > 1000, abort
-
+So I go through n and calculate the number of divisors of n^2 if >2000, abort
 
 Anwser:
     180180
@@ -37,7 +36,21 @@ Anwser:
 '''
 
 import time
-        
+
+def prime_factors(n):
+    factors = []
+    d = 2
+    while n > 1:
+        while n % d == 0:
+            factors.append(d)
+            n /= d
+        d = d + 1
+        if d*d > n:
+            if n > 1: 
+                factors.append(n)
+            break
+    return factors
+
 def Divisors_of_n_squared(n):
     factors = []
     d = 2
@@ -55,17 +68,20 @@ def Divisors_of_n_squared(n):
     divisors = 1
     for x in temp:
         divisors *= (2*factors.count(x) + 1)
-    
+        
+    if divisors % 2 != 0:
+        divisors += 1 #account for a = b case
+        
     return divisors/2
 
-def compute():
+def compute(limit):
     x = 1
     while True:
-        if Divisors_of_n_squared(x) > 1000:
+        if Divisors_of_n_squared(x) > limit:
             return x
         x += 1
         
 if __name__ == "__main__":
     start_time = time.time()
-    print(compute())
+    print(compute(1000))
     print("--- %s seconds ---" % (time.time() - start_time))
