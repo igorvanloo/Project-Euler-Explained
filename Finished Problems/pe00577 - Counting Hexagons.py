82bac1eba3767c69c3de7536a9702b25a65cea75
igorvanloo <igorvanloo@gmail.com>
2021-12-10 23:11:11 +0800

    Finished Problem 577

---
+++ Finished Problems/pe00577 - Counting Hexagons.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 21:06:20 2021
+
+@author: igorvanloo
+"""
+
+'''
+Project Euler Problem 577
+
+H(3) = 1 = T(1)
+H(4) = 3 = T(2)
+H(5) = 6 = T(3)
+H(6) = 12 = T(4) + 2T(1)
+H(7) = 21 = T(5) + 2T(2)
+
+H(20) = T(18) + 2T(15) + 3T(12) + 4T(9) + 5T(6) + 6T(3)
+
+In general every time H(3n) we add n new types of hexagons
+
+H(3) we have a 1 side length hexagons
+H(6) we have 2 side length and also sqrt(3) side length 
+H(9) we have 3, 
+
+
+Anwser:
+    265695031399260211
+--- 9.085942029953003 seconds ---
+'''
+
+import time
+start_time = time.time()
+
+def tri_num(n):
+    return int((n*(n+1))/2)
+
+def compute(limit):
+    total = 0
+    
+    for x in range(3, limit + 1):
+        temp_total = 0
+        for y in range(1, x//3 + 1):
+            temp_total += y*tri_num(x - 2 - 3*(y-1))
+        total += temp_total
+    
+    return total
+        
+if __name__ == "__main__":
+    print(compute(12345))
+    print("--- %s seconds ---" % (time.time() - start_time))
