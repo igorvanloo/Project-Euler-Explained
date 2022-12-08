40150c19305b167040ba5ee6be59001f4cdc7afd
Igor <igorvanloo@gmail.com>
2022-12-08 23:26:37 +0800

    Improved Code

--- Finished Problems/pe00193 - Square Free Numbers.py
+++ Finished Problems/pe00193 - Square Free Numbers.py
@@ -15,9 +15,39 @@ Approximate anwser = 6*2**50 / (math.pi)**2 = 684465067344555
 
 Anwser:
     684465067343069
---- 110.54865097999573 seconds ---
+--- 42.56223487854004 seconds ---
 '''
 import time, math
+
+def mobius_k_sieve(n, k):
+    isprime = [1]*(n + 1)
+    isprime[0] = isprime[1] = 0
+    mob = [0] + [1]*(n)
+    for p in range(2, n + 1):
+        if isprime[p]:
+            mob[p] *= -1
+            for i in range(2*p, n + 1, p):
+                isprime[i] = 0
+                mob[i] *= -1
+            sq = pow(p, k)
+            if sq <= n:
+                for j in range(sq, n + 1, sq):
+                    mob[j] = 0
+    return isprime, mob
+
+def count_kfree(n, k):
+    '''
+    I re-defined the the Mobius function:
+                    1 if n is kpower-free positive integer with even number of prime factors
+        μ_{k}(n) = -1 if n is kpower-free positive integer with odd number of prime factors
+                    0 if n has a k power factor
+                    
+    Computes the number of integers x <= n such that x is k-free, denote this as S(n)
+    We use the fact that S(n) = sum_{d = 1}^n |μ_k(d)| = sum_{d = 1}^{floor{n^(1/k)}} μ_{k}(d)*floor{n/d^k}
+    '''
+    sq = int(pow(n, 1/k))
+    _, mobius_k = mobius_k_sieve(sq, k)
+    return sum([mobius_k[i]*(n//pow(i, k)) for i in range(1, sq + 1)])
 
 def S1(N): #Reduced speed for 10^8 ~ 0.02 seconds, can be used to solve final problem
     sqrtN = int(math.sqrt(N))
@@ -30,5 +60,5 @@ def S1(N): #Reduced speed for 10^8 ~ 0.02 seconds, can be used to solve final pr
 
 if __name__ == "__main__":
     start_time = time.time()
-    print(S1(2**50))
+    print(count_kfree(2**50, 2))
     print("--- %s seconds ---" % (time.time() - start_time))
