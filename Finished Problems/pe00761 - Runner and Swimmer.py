12192462eeb59b0b43d4f822240d5c5d11c7124e
Igor <igorvanloo@gmail.com>
2023-06-27 19:04:24 +0800

    Finished Problem 761

--- Finished Problems/pe00761 - Runner and Swimmer.py
+++ Finished Problems/pe00761 - Runner and Swimmer.py
@@ -14,22 +14,16 @@ Anwser:
 import time
 from sympy import sin, cos, tan, pi, acos
 start_time = time.time()
-
-def find_K(n):
-    theta = pi/n
-    K = lambda k : sin(k*theta) - (k + n)*tan(theta)*cos(k*theta)
-    
-    for k in range(n + 1):
-        if K(k) < 0 < K(k + 1):
-            return k
-
-def find_a(n):
-    K = find_K(n)
-    theta = pi/n
-    return (K*theta + acos(2*sin(K*theta)/((K + n)*tan(theta)) -cos(K*theta)))/2
     
 def compute(n):
-    a = find_a(n)
+    K = 0
+    theta = pi/n
+    f = lambda k : sin(k*theta) - (k + n)*tan(theta)*cos(k*theta)
+    
+    while f(K) < 0:
+        K += 1
+    K -= 1
+    a = (K*theta + acos(2*sin(K*theta)/((K + n)*tan(theta)) -cos(K*theta)))/2
     return round(1/cos(a), 8)
 
 if __name__ == "__main__":
