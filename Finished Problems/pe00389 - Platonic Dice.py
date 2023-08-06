6d7a2e435e59355868700090b09377903ee99629
Igor <igorvanloo@gmail.com>
2023-08-07 01:12:25 +0800

    Finished Problem 389

---
+++ Finished Problems/pe00389 - Platonic Dice.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  7 01:07:54 2023
+
+@author: igorvanloo
+"""
+'''
+Project Euler Problem 389
+
+https://en.wikipedia.org/wiki/Sicherman_dice
+https://en.wikipedia.org/wiki/Probability-generating_function
+
+Anwser:
+    2406376.3623
+    0 seconds, I did it on paper see my website
+'''
+import time
+start_time = time.time()
+
+if __name__ == "__main__":
+    print(2406376.3623)
+    print("--- %s seconds ---" % (time.time() - start_time))
