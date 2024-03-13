52d109482d8b34baf11abcdadda0064cda6fdf3b
Igor <igorvanloo@gmail.com>
2024-03-13 12:42:20 +0800

    Update pe00124 - Ordered Radicals.py
    After solving 127 there is a much faster method

--- Finished Problems/pe00124 - Ordered Radicals.py
+++ Finished Problems/pe00124 - Ordered Radicals.py
@@ -5,48 +5,22 @@ Created on Sun May  9 18:06:59 2021
 
 @author: igorvanloo
 """
-
 '''
 Project Euler Problem 124
 
 Very simple, just create a function to find prime factors and to find rad, append them to a list, sort it and return the 9999th element
-Anwser:
+
+Update: After solving 127 much faster method. From 2.5s to 0.44s
+
+Answer:
     (1947, 21417)
---- 2.7997658252716064 seconds ---
-    
+--- 0.4394040107727051 seconds ---
 '''
 
-import time, math, eulerlib, itertools
+import time
 start_time = time.time()
 
-primes = eulerlib.primes(10**5)
-
-def is_prime(x): #Test if giving value is a prime 
-	if x <= 1:
-		return False
-	elif x <= 3:
-		return True
-	elif x % 2 == 0:
-		return False
-	else:
-		for i in range(3, int(math.sqrt(x)) + 1, 2):
-			if x % i == 0:
-				return False
-		return True
-    
-def primefactorization(n, listofprimes): #Requires a preloaded list of primes
-    if is_prime(n) == True:
-        return [n]
-    else:
-        factors = []
-        while n != 1:
-            for x in listofprimes:
-                if n % x == 0:
-                    factors.append(x)
-                    n = n/x
-                    break
-        return list(set(factors))
-
+### Method 1 - Original Method ###
 def prime_factors(n):
     factors = []
     d = 2
@@ -67,14 +41,35 @@ def rad(factors):
         totalsum *= x
     return totalsum
     
-def compute():
+def compute(limit_n, goal):
     finallist = []
-    for x in range(1, 100000 + 1):
+    for x in range(1, limit_n + 1):
         finallist.append([rad(prime_factors(x)), x])
     final = sorted(finallist)
-    return final[9999]
-        
+    return final[goal - 1]
+
+### Method 2 - After solving 127 ###
+#[1947, 21417]
+#--- 0.4394040107727051 seconds ---
+def gen_rad(n):
+    rad = [1]*(n + 1)
+    rad[0] = 0
+    rad[1] = 1
+    for i in range(2, n + 1):
+        if rad[i] == 1: #This is a prime
+            for j in range(i, n + 1, i):
+                rad[j] *= i
+    return rad
+
+def compute1(limit_n, goal):
+    finallist = []
+    rad = gen_rad(limit_n)
+    for x in range(1, limit_n + 1):
+        finallist.append([rad[x], x])
+    final = sorted(finallist)
+    return final[goal - 1]
+
 
 if __name__ == "__main__":
-    print(compute())
+    print(compute(100000, 10000))
     print("--- %s seconds ---" % (time.time() - start_time))
