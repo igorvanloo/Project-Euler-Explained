c66d3ca3c3748978ea05c4b4748e737d36d2e667
Igor <igorvanloo@gmail.com>
2024-12-19 17:43:40 +0800

    Added bonus problems, solved Problem -1

---
+++ Bonus Problems/Problem -1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 16:42:02 2024
+
+@author: Igor Van Loo
+"""
+'''
+Project Euler Bonus Problem -1
+
+Using 1 + 2 + 3 + 4 + ... = -1/12
+
+Following same logic as Problem 1 we have
+-3/12 - 5/12 + 15/12 = 7/12
+
+Answer:
+    7/12
+'''
